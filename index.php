dfb61686a128f22abac9e15de4ba174811fa310d
Khanh <kupigel29@gmail.com>
2021-11-02 00:04:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Minh Hiệp Protect</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,500;1,100;1,300&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="./styles.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</head>
+<body>
+	<div id="header">
+		<div class="container">
+			<div class="row">
+				<img class="header-logo col-lg-2" src="./img/logo.jpg">
+				<h1 class="col-lg-7">Công Ty Phân Phối, Lắp Đặt Camera An Ninh Giải Pháp Phần Mềm</h1>
+				<img class="col-lg-3" src="./img/header.png" style="height:80px ;">
+			</div>
+		</div>
+	</div>
+	<div id="menu-nav">
+	<nav class="navbar navbar-expand-sm bg-blue">
+	  <!-- Links -->
+	  <div class="container">
+	  	<div class="row">
+		  <ul class="navbar-nav col-lg-8">
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">Trang Chủ</a>
+		    </li>
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">Thông Tin</a>
+		    </li>
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">Liên Hệ</a>
+		    </li>
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">Sản Phẩm</a>
+		    </li>
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">Công Trình</a>
+		    </li>
+		  </ul>
+		   <form class="form-inline col-lg-4" action="/action_page.php">
+		    <input class="form-control col-lg-8" type="text" placeholder="Search">
+		    <button class="btn btn-warning col-lg-4" type="submit">Search</button>
+		 </form>
+	 	</div>
+	  </div>
+	</nav>
+	<br>
+	</div>
+	<div id="main">
+		
+	</div>	
+</body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+    },
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
